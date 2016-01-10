1c4376caf4de7effc84ef59f6b2c257f1698b32f
Vasco Lavos <vazkii@hotmail.com>
2016-01-10 23:17:16 +0000

    Better jsoning scripts

--- src/main/resources/assets/psi/models/block/_make.bat
+++ src/main/resources/assets/psi/models/block/_make.bat
@@ -5,7 +5,7 @@
 :: Can make multiple blocks at once
 ::
 :: Usage:
-:: _make (blocks name 1) (blocks name 2) (blocks name x)
+:: _make (block name 1) (block name 2) (block name x)
 ::
 :: Change this to your mod's ID
 set modid=psi
@@ -16,31 +16,38 @@ for %%x in (%*) do (
 	echo Making %%x.json block
 	(
 		echo {
-		echo     "parent": "block/cube_all",
-		echo     "textures": {
-		echo         "all": "%modid%:blocks/%%x"
-		echo     }
+		echo 	"parent": "block/cube_all",
+		echo 	"textures": {
+		echo 		"all": "%modid%:blocks/%%x"
+		echo 	}
 		echo }
 	) > %%x.json
 
 	echo Making %%x.json item
 	(
 		echo {
-	    echo     "parent": "%modid%:block/%%x"
+		echo 	"parent": "%modid%:block/%%x"
+		echo 	"display": {
+		echo 		"thirdperson": {
+		echo 			"rotation": [ 10, -45, 170 ],
+		echo 			"translation": [ 0, 1.5, -2.75 ],
+		echo 			"scale": [ 0.375, 0.375, 0.375 ]
+		echo 		}
+		echo 	}
 		echo }
 	) > ../item/%%x.json
 
 	echo Making %%x.json blockstate
 	(
 		echo {
-		echo   "forge_marker": 1,
-		echo   "defaults": {
-		echo     "model": "%modid%:%%x"
-		echo   },
-		echo   "variants": {
-		echo     "normal": [{}],
-		echo     "inventory": [{}]
-		echo   }
+		echo 	"forge_marker": 1,
+		echo 	"defaults": {
+		echo 		"model": "%modid%:%%x"
+		echo 	},
+		echo 	"variants": {
+		echo 		"normal": [{}],
+		echo 		"inventory": [{}]
+		echo 	}
 		echo }
 	) > ../../blockstates/%%x.json
 )
