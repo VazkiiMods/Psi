8c83f67dfcc844e6fc1d9f484641e519d66623d8
Vasco Lavos <vazkii@hotmail.com>
2016-02-10 03:24:08 +0000

    Remove a from the API version

--- src/main/java/vazkii/psi/api/package-info.java
+++ src/main/java/vazkii/psi/api/package-info.java
@@ -1,4 +1,4 @@
-@API(owner = "Psi", apiVersion = "a1", provides = "PsiAPI")
+@API(owner = "Psi", apiVersion = "1", provides = "PsiAPI")
 package vazkii.psi.api;
 import net.minecraftforge.fml.common.API;
 
