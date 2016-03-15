2e4fbc9b1aa6287c5d8e85406b88f6caf2a682f9
Vasco Lavos <vazkii@hotmail.com>
2016-03-15 17:54:39 +0000

    It loads! Depends on https://github.com/MinecraftForge/MinecraftForge/pull/2544

--- src/main/java/vazkii/psi/api/PsiAPI.java
+++ src/main/java/vazkii/psi/api/PsiAPI.java
@@ -13,6 +13,7 @@ package vazkii.psi.api;
 import java.util.HashMap;
 
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.SoundEvents;
 import net.minecraft.item.Item.ToolMaterial;
 import net.minecraft.item.ItemArmor.ArmorMaterial;
 import net.minecraft.item.ItemStack;
@@ -44,7 +45,7 @@ public final class PsiAPI {
 	public static HashMap<String, PieceGroup> groupsForName = new HashMap();
 
 	public static ToolMaterial PSIMETAL_TOOL_MATERIAL = EnumHelper.addToolMaterial("PSIMETAL", 3, 900, 7.8F, 3F, 12);
-	public static ArmorMaterial PSIMETAL_ARMOR_MATERIAL = EnumHelper.addArmorMaterial("PSIMETAL", "", 18, new int[]{2, 6, 5, 2}, 12);
+	public static ArmorMaterial PSIMETAL_ARMOR_MATERIAL = EnumHelper.addArmorMaterial("PSIMETAL", "", 18, new int[]{2, 6, 5, 2}, 12, SoundEvents.item_armor_equip_iron);
 
 	public static int levelCap = 1;
 
