90c8694a176fd8e063a60b1b2629dec02cf5320e
yrsegal@gmail.com <yrsegal@gmail.com>
2019-01-05 19:08:50 -0500

    patch over a crash
    pls don't load before your dependencies
    https://github.com/Vazkii/Psi/issues/265#issuecomment-451702166

--- src/main/java/vazkii/psi/common/lib/LibMisc.java
+++ src/main/java/vazkii/psi/common/lib/LibMisc.java
@@ -17,7 +17,7 @@ public final class LibMisc {
 	public static final String MOD_NAME = "Psi";
 	public static final String BUILD = "GRADLE:BUILD";
 	public static final String VERSION = "GRADLE:VERSION-" + BUILD;
-	public static final String DEPENDENCIES = "required-before:autoreglib";
+	public static final String DEPENDENCIES = "required-after:autoreglib";
 	public static final String PREFIX_MOD = MOD_ID + ":";
 	public static final boolean BETA_TESTING = false;
 
