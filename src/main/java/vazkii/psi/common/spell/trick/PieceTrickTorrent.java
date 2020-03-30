a2dfe8af95f45af053158a4de866b9d12f2895f2
Kamefrede <kamefrede@gmail.com>
2020-03-30 02:37:14 +0100

    Add forge event calls to prevent griefing with Psi.

--- src/main/java/vazkii/psi/common/spell/trick/PieceTrickTorrent.java
+++ src/main/java/vazkii/psi/common/spell/trick/PieceTrickTorrent.java
@@ -12,15 +12,13 @@ package vazkii.psi.common.spell.trick;
 
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
+import net.minecraft.util.Direction;
 import net.minecraft.util.math.BlockPos;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.common.util.BlockSnapshot;
+import net.minecraftforge.event.world.BlockEvent;
 import vazkii.psi.api.internal.Vector3;
-import vazkii.psi.api.spell.EnumSpellStat;
-import vazkii.psi.api.spell.Spell;
-import vazkii.psi.api.spell.SpellCompilationException;
-import vazkii.psi.api.spell.SpellContext;
-import vazkii.psi.api.spell.SpellMetadata;
-import vazkii.psi.api.spell.SpellParam;
-import vazkii.psi.api.spell.SpellRuntimeException;
+import vazkii.psi.api.spell.*;
 import vazkii.psi.api.spell.param.ParamVector;
 import vazkii.psi.api.spell.piece.PieceTrick;
 
@@ -51,20 +49,24 @@ public class PieceTrickTorrent extends PieceTrick {
 
 		Vector3 positionVal = this.getParamValue(context, position);
 
-		if(positionVal == null)
+		if (positionVal == null)
 			throw new SpellRuntimeException(SpellRuntimeException.NULL_VECTOR);
-		if(!context.isInRadius(positionVal))
+		if (!context.isInRadius(positionVal))
 			throw new SpellRuntimeException(SpellRuntimeException.OUTSIDE_RADIUS);
 
 		BlockPos pos = positionVal.toBlockPos();
 
 		BlockState state = context.caster.getEntityWorld().getBlockState(pos);
-		if(state.isAir(context.caster.getEntityWorld(), pos) || state.getMaterial().isReplaceable())
+		BlockEvent.EntityPlaceEvent placeEvent = new BlockEvent.EntityPlaceEvent(BlockSnapshot.getBlockSnapshot(context.caster.getEntityWorld(), pos), context.caster.getEntityWorld().getBlockState(pos.offset(Direction.UP)), context.caster);
+		MinecraftForge.EVENT_BUS.post(placeEvent);
+		if (placeEvent.isCanceled())
+			return null;
+		if (state.isAir(context.caster.getEntityWorld(), pos) || state.getMaterial().isReplaceable())
 			context.caster.getEntityWorld().setBlockState(pos, Blocks.WATER.getDefaultState());
 		else {
 			pos = pos.up();
 			state = context.caster.getEntityWorld().getBlockState(pos);
-			if(state.isAir(context.caster.getEntityWorld(), pos) || state.getMaterial().isReplaceable())
+			if (state.isAir(context.caster.getEntityWorld(), pos) || state.getMaterial().isReplaceable())
 				context.caster.getEntityWorld().setBlockState(pos, Blocks.WATER.getDefaultState());
 		}
 
