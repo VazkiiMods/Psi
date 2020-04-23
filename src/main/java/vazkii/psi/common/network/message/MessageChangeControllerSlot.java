774c1f5d5b2370d6542b330005ec9176b0aab95b
Logan Peterson <loganpete12@gmail.com>
2020-04-23 16:16:42 -0600

    Fixed issue #533

--- src/main/java/vazkii/psi/common/network/message/MessageChangeControllerSlot.java
+++ src/main/java/vazkii/psi/common/network/message/MessageChangeControllerSlot.java
@@ -37,7 +37,7 @@ public class MessageChangeControllerSlot implements IMessage {
 			if (!stack.isEmpty() && stack.getItem() instanceof ISocketableController) {
 				((ISocketableController) stack.getItem()).setSelectedSlot(player, stack, controlSlot, slot);
 			} else {
-				stack = player.getHeldItem(Hand.MAIN_HAND);
+				stack = player.getHeldItem(Hand.OFF_HAND);
 				if (!stack.isEmpty() && stack.getItem() instanceof ISocketableController) {
 					((ISocketableController) stack.getItem()).setSelectedSlot(player, stack, controlSlot, slot);
 				}
