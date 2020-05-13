63789251dfe76222950b895469e922ecbf7ca762
Kamefrede <kamefrede@gmail.com>
2020-05-13 14:02:59 +0100

    I hate windows.
    Protip for anyone with windows who wants to run update-book.sh, do it under WSL or it will break the unicode characters.

--- src/main/resources/update-book.sh
+++ src/main/resources/update-book.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-
-python3 convert.py mappings-en_us.json |python3 generate_book.py - en_us_base.json assets/psi/lang/en_us.json
+python3 convert.py mappings-en_us.json |python3 generate_book.py - en_us_base.json > assets/psi/lang/en_us.json
