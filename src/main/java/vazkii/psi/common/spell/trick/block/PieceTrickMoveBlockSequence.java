7d4f5d1291a7f94f7d3245a919a79ca15f58eae4
Kamefrede <kamefrede@gmail.com>
2021-10-16 23:48:30 +0100

    "fix": #690 NOTE: This is the worst fucking piece of code I've ever written. I hate this.

--- src/main/java/vazkii/psi/common/spell/trick/block/PieceTrickMoveBlockSequence.java
+++ src/main/java/vazkii/psi/common/spell/trick/block/PieceTrickMoveBlockSequence.java
@@ -69,50 +69,80 @@ public class PieceTrickMoveBlockSequence extends PieceTrick {
 		Vector3 positionVal = SpellHelpers.getVector3(this, context, position, true, false);
 		Vector3 targetVal = SpellHelpers.getVector3(this, context, target, false, false);
 		int maxBlocksVal = this.getParamValue(context, maxBlocks).intValue();
+		World world = context.caster.world;
 
 		Map<BlockPos, BlockState> toSet = Maps.newHashMap();
 		Map<BlockPos, BlockState> toRemove = Maps.newHashMap();
 
 		Vector3 directNorm = directionVal.copy().normalize();
 		Vector3 targetNorm = targetVal.copy().normalize();
+
 		LinkedHashSet<BlockPos> positions = MathHelper.getBlocksAlongRay(positionVal.toVec3D(), positionVal.copy().add(targetNorm.copy().multiply(maxBlocksVal)).toVec3D(), maxBlocksVal);
+		LinkedHashSet<BlockPos> moveableBlocks = new LinkedHashSet<>();
+		LinkedHashSet<BlockPos> immovableBlocks = new LinkedHashSet<>();
+
 		for (BlockPos blockPos : positions) {
-			World world = context.caster.world;
 			BlockState state = world.getBlockState(blockPos);
+
+			if (world.isAirBlock(blockPos)) {
+				continue;
+			}
 
 			if (world.getTileEntity(blockPos) != null ||
 					state.getPushReaction() != PushReaction.NORMAL ||
-					state.getPlayerRelativeBlockHardness(context.caster, world, blockPos) <= 0 ||
-					!PieceTrickBreakBlock.canHarvestBlock(state, context.caster, world, blockPos, context.tool) ||
+					state.getBlockHardness(world, blockPos) == -1 ||
+					!PieceTrickBreakBlock.canHarvestBlock(state, context.caster, world, blockPos, context.getHarvestTool()) ||
 					!SpellHelpers.isBlockPosInRadius(context, blockPos) ||
-					!world.isBlockModifiable(context.caster, blockPos) ||
-					world.isAirBlock(blockPos)) {
+					!world.isBlockModifiable(context.caster, blockPos)) {
+				immovableBlocks.add(blockPos);
 				continue;
 			}
 
 			BlockPos pushToPos = blockPos.add(directNorm.x, directNorm.y, directNorm.z);
-			BlockPos nextPos = blockPos.add(targetNorm.x, targetNorm.y, targetNorm.z);
-			BlockState pushToState = world.getBlockState(pushToPos);
-			BlockPos nextPosPushPos = nextPos.add(directNorm.x, directNorm.y, directNorm.z);
-			if (!world.isBlockModifiable(context.caster, pushToPos)) {
+			boolean isOffWorld = pushToPos.getY() < 0 || pushToPos.getY() > 256;
+			if (isOffWorld) {
+				immovableBlocks.add(blockPos);
 				continue;
 			}
 
 			BlockEvent.BreakEvent event = new BlockEvent.BreakEvent(world, blockPos, state, context.caster);
 
 			if (MinecraftForge.EVENT_BUS.post(event)) {
+				immovableBlocks.add(blockPos);
 				continue;
 			}
+			moveableBlocks.add(blockPos);
+		}
 
-			if ((nextPos.equals(pushToPos) &&
-					((positions.contains(nextPos) && (world.isAirBlock(nextPosPushPos) || world.getBlockState(nextPosPushPos).getMaterial().isReplaceable() || positions.contains(nextPosPushPos))))) ||
-					(world.isAirBlock(pushToPos) || pushToState.getMaterial().isReplaceable()) &&
-							!(pushToPos.getY() < 0 || pushToPos.getY() > 256)) {
-				toRemove.put(blockPos, state);
-				toSet.put(pushToPos, state);
-			} else {
-				return null;
+		outer:
+		for (BlockPos blockPos : moveableBlocks) {
+			BlockState state = world.getBlockState(blockPos);
+			BlockPos pushToPos = blockPos.add(directNorm.x, directNorm.y, directNorm.z);
+			BlockState pushToState = world.getBlockState(pushToPos);
+			if (immovableBlocks.contains(pushToPos) || immovableBlocks.contains(blockPos)) {
+				continue;
 			}
+			if (moveableBlocks.contains(pushToPos)) {
+				BlockPos nextPos = pushToPos;
+				while (moveableBlocks.contains(nextPos)) {
+					BlockPos nextPosPushPos = nextPos.add(directNorm.x, directNorm.y, directNorm.z);
+					BlockState nextPosPushPosState = world.getBlockState(nextPosPushPos);
+
+					if (moveableBlocks.contains(nextPosPushPos)) {
+						nextPos = nextPosPushPos;
+						continue;
+					}
+
+					if (immovableBlocks.contains(nextPosPushPos) || !(world.isAirBlock(nextPosPushPos) || nextPosPushPosState.getMaterial().isReplaceable())) {
+						continue outer;
+					}
+					break;
+				}
+			} else if (!(world.isAirBlock(pushToPos) || pushToState.getMaterial().isReplaceable())) {
+				continue;
+			}
+			toRemove.put(blockPos, state);
+			toSet.put(pushToPos, state);
 		}
 
 		for (Map.Entry<BlockPos, BlockState> pairtoRemove : toRemove.entrySet()) {
