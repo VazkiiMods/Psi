391a3c04973576a0929819ad222394fb0cbaaeeb
panda885 <firegox@email.cz>
2023-03-29 15:54:47 +0200

    fix: use new forge registry api for blocks

--- src/main/java/vazkii/psi/common/block/base/ModBlocks.java
+++ src/main/java/vazkii/psi/common/block/base/ModBlocks.java
@@ -9,10 +9,10 @@
 package vazkii.psi.common.block.base;
 
 import net.minecraft.client.gui.screens.MenuScreens;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.inventory.MenuType;
 import net.minecraft.world.item.BlockItem;
-import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Rarity;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.SoundType;
@@ -21,12 +21,12 @@ import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.material.Material;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.extensions.IForgeMenuType;
-import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.registries.IForgeRegistry;
+import net.minecraftforge.registries.ForgeRegistries;
 
+import net.minecraftforge.registries.RegisterEvent;
 import vazkii.psi.client.gui.GuiCADAssembler;
 import vazkii.psi.common.block.BlockCADAssembler;
 import vazkii.psi.common.block.BlockConjured;
@@ -61,65 +61,60 @@ public class ModBlocks {
 	public static final MenuType<ContainerCADAssembler> containerCADAssembler = IForgeMenuType.create(ContainerCADAssembler::fromNetwork);
 
 	@SubscribeEvent
-	public static void registerBlocks(RegistryEvent.Register<Block> evt) {
-		cadAssembler = new BlockCADAssembler(Block.Properties.of(Material.METAL).strength(5, 10).sound(SoundType.METAL).noOcclusion());
-		programmer = new BlockProgrammer(Block.Properties.of(Material.METAL).strength(5, 10).sound(SoundType.METAL).noOcclusion());
-		conjured = new BlockConjured(Block.Properties.of(Material.GLASS).noDrops().lightLevel(state -> state.getValue(BlockConjured.LIGHT) ? 15 : 0).noOcclusion().isValidSpawn(NO_SPAWN).isRedstoneConductor(NO_SUFFOCATION).isSuffocating(NO_SUFFOCATION).isViewBlocking(NO_SUFFOCATION));
-		psidustBlock = new Block(Block.Properties.of(Material.METAL).strength(5, 10).sound(SoundType.METAL));
-		psimetalBlock = new Block(Block.Properties.of(Material.METAL).strength(5, 10).sound(SoundType.METAL));
-		psigemBlock = new Block(Block.Properties.of(Material.METAL).strength(5, 10).sound(SoundType.METAL));
-		psimetalPlateBlack = new Block(Block.Properties.of(Material.METAL).strength(5, 10).sound(SoundType.METAL));
-		psimetalPlateBlackLight = new Block(Block.Properties.of(Material.METAL).strength(5, 10).sound(SoundType.METAL).lightLevel((blockState) -> 15));
-		psimetalPlateWhite = new Block(Block.Properties.of(Material.METAL).strength(5, 10).sound(SoundType.METAL));
-		psimetalPlateWhiteLight = new Block(Block.Properties.of(Material.METAL).strength(5, 10).sound(SoundType.METAL).lightLevel((blockstate) -> 15));
-		psimetalEbony = new Block(Block.Properties.of(Material.METAL).strength(5, 10).sound(SoundType.METAL));
-		psimetalIvory = new Block(Block.Properties.of(Material.METAL).strength(5, 10).sound(SoundType.METAL));
+	public static void register(RegisterEvent evt) {
+		evt.register(ForgeRegistries.Keys.BLOCKS, helper -> {
+			cadAssembler = new BlockCADAssembler(Block.Properties.of(Material.METAL).strength(5, 10).sound(SoundType.METAL).noOcclusion());
+			programmer = new BlockProgrammer(Block.Properties.of(Material.METAL).strength(5, 10).sound(SoundType.METAL).noOcclusion());
+			conjured = new BlockConjured(Block.Properties.of(Material.GLASS).noLootTable().lightLevel(state -> state.getValue(BlockConjured.LIGHT) ? 15 : 0).noOcclusion().isValidSpawn(NO_SPAWN).isRedstoneConductor(NO_SUFFOCATION).isSuffocating(NO_SUFFOCATION).isViewBlocking(NO_SUFFOCATION));
+			psidustBlock = new Block(Block.Properties.of(Material.METAL).strength(5, 10).sound(SoundType.METAL));
+			psimetalBlock = new Block(Block.Properties.of(Material.METAL).strength(5, 10).sound(SoundType.METAL));
+			psigemBlock = new Block(Block.Properties.of(Material.METAL).strength(5, 10).sound(SoundType.METAL));
+			psimetalPlateBlack = new Block(Block.Properties.of(Material.METAL).strength(5, 10).sound(SoundType.METAL));
+			psimetalPlateBlackLight = new Block(Block.Properties.of(Material.METAL).strength(5, 10).sound(SoundType.METAL).lightLevel((blockState) -> 15));
+			psimetalPlateWhite = new Block(Block.Properties.of(Material.METAL).strength(5, 10).sound(SoundType.METAL));
+			psimetalPlateWhiteLight = new Block(Block.Properties.of(Material.METAL).strength(5, 10).sound(SoundType.METAL).lightLevel((blockstate) -> 15));
+			psimetalEbony = new Block(Block.Properties.of(Material.METAL).strength(5, 10).sound(SoundType.METAL));
+			psimetalIvory = new Block(Block.Properties.of(Material.METAL).strength(5, 10).sound(SoundType.METAL));
 
-		IForgeRegistry<Block> r = evt.getRegistry();
-		r.register(cadAssembler.setRegistryName(LibMisc.MOD_ID, LibBlockNames.CAD_ASSEMBLER));
-		r.register(programmer.setRegistryName(LibMisc.MOD_ID, LibBlockNames.PROGRAMMER));
-		r.register(conjured.setRegistryName(LibMisc.MOD_ID, LibBlockNames.CONJURED));
-		r.register(psidustBlock.setRegistryName(LibMisc.MOD_ID, LibBlockNames.PSIDUST_BLOCK));
-		r.register(psimetalBlock.setRegistryName(LibMisc.MOD_ID, LibBlockNames.PSIMETAL_BLOCK));
-		r.register(psigemBlock.setRegistryName(LibMisc.MOD_ID, LibBlockNames.PSIGEM_BLOCK));
-		r.register(psimetalPlateBlack.setRegistryName(LibMisc.MOD_ID, LibBlockNames.PSIMETAL_PLATE_BLACK));
-		r.register(psimetalPlateBlackLight.setRegistryName(LibMisc.MOD_ID, LibBlockNames.PSIMETAL_PLATE_BLACK_LIGHT));
-		r.register(psimetalPlateWhite.setRegistryName(LibMisc.MOD_ID, LibBlockNames.PSIMETAL_PLATE_WHITE));
-		r.register(psimetalPlateWhiteLight.setRegistryName(LibMisc.MOD_ID, LibBlockNames.PSIMETAL_PLATE_WHITE_LIGHT));
-		r.register(psimetalEbony.setRegistryName(LibMisc.MOD_ID, LibBlockNames.EBONY_PSIMETAL_BLOCK));
-		r.register(psimetalIvory.setRegistryName(LibMisc.MOD_ID, LibBlockNames.IVORY_PSIMETAL_BLOCK));
-	}
+			helper.register(new ResourceLocation(LibMisc.MOD_ID, LibBlockNames.CAD_ASSEMBLER), cadAssembler);
+			helper.register(new ResourceLocation(LibMisc.MOD_ID, LibBlockNames.PROGRAMMER), programmer);
+			helper.register(new ResourceLocation(LibMisc.MOD_ID, LibBlockNames.CONJURED), conjured);
+			helper.register(new ResourceLocation(LibMisc.MOD_ID, LibBlockNames.PSIDUST_BLOCK), psidustBlock);
+			helper.register(new ResourceLocation(LibMisc.MOD_ID, LibBlockNames.PSIMETAL_BLOCK), psimetalBlock);
+			helper.register(new ResourceLocation(LibMisc.MOD_ID, LibBlockNames.PSIGEM_BLOCK), psigemBlock);
+			helper.register(new ResourceLocation(LibMisc.MOD_ID, LibBlockNames.PSIMETAL_PLATE_BLACK), psimetalPlateBlack);
+			helper.register(new ResourceLocation(LibMisc.MOD_ID, LibBlockNames.PSIMETAL_PLATE_BLACK_LIGHT), psimetalPlateBlackLight);
+			helper.register(new ResourceLocation(LibMisc.MOD_ID, LibBlockNames.PSIMETAL_PLATE_WHITE), psimetalPlateWhite);
+			helper.register(new ResourceLocation(LibMisc.MOD_ID, LibBlockNames.PSIMETAL_PLATE_WHITE_LIGHT), psimetalPlateWhiteLight);
+			helper.register(new ResourceLocation(LibMisc.MOD_ID, LibBlockNames.EBONY_PSIMETAL_BLOCK), psimetalBlock);
+			helper.register(new ResourceLocation(LibMisc.MOD_ID, LibBlockNames.IVORY_PSIMETAL_BLOCK), psimetalIvory);
+		});
 
-	@SubscribeEvent
-	public static void registerItemBlocks(RegistryEvent.Register<Item> evt) {
-		IForgeRegistry<Item> r = evt.getRegistry();
-		r.register(new BlockItem(cadAssembler, defaultBuilder().rarity(Rarity.UNCOMMON)).setRegistryName(cadAssembler.getRegistryName()));
-		r.register(new BlockItem(programmer, defaultBuilder().rarity(Rarity.UNCOMMON)).setRegistryName(programmer.getRegistryName()));
-		r.register(new BlockItem(psidustBlock, defaultBuilder()).setRegistryName(psidustBlock.getRegistryName()));
-		r.register(new BlockItem(psimetalBlock, defaultBuilder()).setRegistryName(psimetalBlock.getRegistryName()));
-		r.register(new BlockItem(psigemBlock, defaultBuilder()).setRegistryName(psigemBlock.getRegistryName()));
-		r.register(new BlockItem(psimetalPlateBlack, defaultBuilder()).setRegistryName(psimetalPlateBlack.getRegistryName()));
-		r.register(new BlockItem(psimetalPlateBlackLight, defaultBuilder()).setRegistryName(psimetalPlateBlackLight.getRegistryName()));
-		r.register(new BlockItem(psimetalPlateWhite, defaultBuilder()).setRegistryName(psimetalPlateWhite.getRegistryName()));
-		r.register(new BlockItem(psimetalPlateWhiteLight, defaultBuilder()).setRegistryName(psimetalPlateWhiteLight.getRegistryName()));
-		r.register(new BlockItem(psimetalEbony, defaultBuilder()).setRegistryName(psimetalEbony.getRegistryName()));
-		r.register(new BlockItem(psimetalIvory, defaultBuilder()).setRegistryName(psimetalIvory.getRegistryName()));
-	}
+		evt.register(ForgeRegistries.Keys.ITEMS, helper -> {
+			helper.register(ForgeRegistries.BLOCKS.getKey(cadAssembler), new BlockItem(cadAssembler, defaultBuilder().rarity(Rarity.UNCOMMON)));
+			helper.register(ForgeRegistries.BLOCKS.getKey(programmer), new BlockItem(programmer, defaultBuilder().rarity(Rarity.UNCOMMON)));
+			helper.register(ForgeRegistries.BLOCKS.getKey(psidustBlock), new BlockItem(psidustBlock, defaultBuilder()));
+			helper.register(ForgeRegistries.BLOCKS.getKey(psimetalBlock), new BlockItem(psimetalBlock, defaultBuilder()));
+			helper.register(ForgeRegistries.BLOCKS.getKey(psigemBlock), new BlockItem(psigemBlock, defaultBuilder()));
+			helper.register(ForgeRegistries.BLOCKS.getKey(psimetalPlateBlack), new BlockItem(psimetalPlateBlack, defaultBuilder()));
+			helper.register(ForgeRegistries.BLOCKS.getKey(psimetalPlateBlackLight), new BlockItem(psimetalPlateBlackLight, defaultBuilder()));
+			helper.register(ForgeRegistries.BLOCKS.getKey(psimetalPlateWhite), new BlockItem(psimetalPlateWhite, defaultBuilder()));
+			helper.register(ForgeRegistries.BLOCKS.getKey(psimetalPlateWhiteLight), new BlockItem(psimetalPlateWhiteLight, defaultBuilder()));
+			helper.register(ForgeRegistries.BLOCKS.getKey(psimetalEbony), new BlockItem(psimetalEbony, defaultBuilder()));
+			helper.register(ForgeRegistries.BLOCKS.getKey(psimetalIvory), new BlockItem(psimetalIvory, defaultBuilder()));
+		});
 
-	@SubscribeEvent
-	public static void initTileEntities(RegistryEvent.Register<BlockEntityType<?>> evt) {
-		IForgeRegistry<BlockEntityType<?>> r = evt.getRegistry();
-		r.register(BlockEntityType.Builder.of(TileCADAssembler::new, cadAssembler).build(null).setRegistryName(cadAssembler.getRegistryName()));
-		r.register(BlockEntityType.Builder.of(TileProgrammer::new, programmer).build(null).setRegistryName(programmer.getRegistryName()));
-		r.register(BlockEntityType.Builder.of(TileConjured::new, conjured).build(null).setRegistryName(conjured.getRegistryName()));
-	}
+		evt.register(ForgeRegistries.Keys.BLOCK_ENTITY_TYPES, helper -> {
+			helper.register(ForgeRegistries.BLOCKS.getKey(cadAssembler), BlockEntityType.Builder.of(TileCADAssembler::new, cadAssembler).build(null));
+			helper.register(ForgeRegistries.BLOCKS.getKey(programmer), BlockEntityType.Builder.of(TileProgrammer::new, programmer).build(null));
+			helper.register(ForgeRegistries.BLOCKS.getKey(conjured), BlockEntityType.Builder.of(TileConjured::new, conjured).build(null));
+		});
 
-	@SubscribeEvent
-	public static void registerContainers(RegistryEvent.Register<MenuType<?>> evt) {
-		evt.getRegistry().register(containerCADAssembler.setRegistryName(LibMisc.MOD_ID, LibBlockNames.CAD_ASSEMBLER));
-		DistExecutor.runWhenOn(Dist.CLIENT, () -> () -> {
-			MenuScreens.register(containerCADAssembler, GuiCADAssembler::new);
+		evt.register(ForgeRegistries.Keys.MENU_TYPES, helper -> {
+			helper.register(ForgeRegistries.BLOCKS.getKey(cadAssembler), containerCADAssembler);
+			DistExecutor.runWhenOn(Dist.CLIENT, () -> () -> {
+				MenuScreens.register(containerCADAssembler, GuiCADAssembler::new);
+			});
 		});
 	}
-
 }
