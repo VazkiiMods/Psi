2012b3a9852f043149573ece46cb332cfef90929
Emanuel Ferraz <kamefrede@gmail.com>
2025-04-27 22:52:44 +0100

    fix python script formatting

--- scripts/push_tag.py
+++ scripts/push_tag.py
@@ -2,43 +2,53 @@ import os
 import re
 from jproperties import Properties
 
+
 def main():
-	build = Properties()
-	with open('build.properties', 'rb') as f:
-		build.load(f , "utf-8")
+    build = Properties()
+    with open("build.properties", "rb") as f:
+        build.load(f, "utf-8")
 
-	mc_version, mcv_meta = build['mc_version']
-	version, v_meta = build['version']
-	build_number, bn_meta = build['build_number']
+    mc_version, mcv_meta = build["mc_version"]
+    version, v_meta = build["version"]
+    build_number, bn_meta = build["build_number"]
 
-	print('MC Version:', mc_version)
-	print('Version:', version)
-	print('Build Number', build_number)
+    print("MC Version:", mc_version)
+    print("Version:", version)
+    print("Build Number", build_number)
 
-	changelog = ''
-	with open('changelog.txt', 'r') as f:
-		content = f.read()
-		content = content.replace('"', '\'')
-		lines = content.splitlines()
-		for line in lines[1:]:
+    changelog = ""
+    with open("changelog.txt", "r") as f:
+        content = f.read()
+        content = content.replace('"', "'")
+        lines = content.splitlines()
+        for line in lines[1:]:
             if not line.startswith("*"):
-            	break
-			changelog = changelog + '-m "'+line+'" '
+                break
+            changelog = changelog + '-m "' + line + '" '
 
-	tag_success = os.system('git tag -a release-{}-{}-{} {}'.format(mc_version, version, build_number, changelog))
+    tag_success = os.system(
+        "git tag -a release-{}-{}-{} {}".format(
+            mc_version, version, build_number, changelog
+        )
+    )
 
-	if tag_success != 0:
-		print('Failed to create tag')
-		return
-	else :
-		print('Created tag')
+    if tag_success != 0:
+        print("Failed to create tag")
+        return
+    else:
+        print("Created tag")
 
-	build['build_number'] = str(int(build_number) + 1)
-	with open("build.properties", "wb") as f:
-		build.store(f, encoding="utf-8")
+    build["build_number"] = str(int(build_number) + 1)
+    with open("build.properties", "wb") as f:
+        build.store(f, encoding="utf-8")
 
-	os.system('git commit -a -m build')
-	os.system('git push origin master release-{}-{}-{}'.format(mc_version, version, build_number))
+    os.system("git commit -a -m build")
+    os.system(
+        "git push origin master release-{}-{}-{}".format(
+            mc_version, version, build_number
+        )
+    )
 
-if __name__ == '__main__':
-	main()
+
+if __name__ == "__main__":
+    main()
