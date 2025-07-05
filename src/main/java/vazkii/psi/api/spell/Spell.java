04149baeb8b564f25901c72407a89e824c98ff9c
Qther <qther@tuta.io>
2025-07-05 18:10:49 +0800

    stream codecs for Spell, SpellGrid, and SpellPiece

--- src/main/java/vazkii/psi/api/spell/Spell.java
+++ src/main/java/vazkii/psi/api/spell/Spell.java
@@ -17,10 +17,12 @@ import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.ListTag;
 import net.minecraft.network.RegistryFriendlyByteBuf;
+import net.minecraft.network.codec.ByteBufCodecs;
 import net.minecraft.network.codec.StreamCodec;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.api.distmarker.OnlyIn;
 import net.neoforged.fml.ModList;
+import net.neoforged.neoforge.network.codec.NeoForgeStreamCodecs;
 
 import org.jetbrains.annotations.Nullable;
 
@@ -37,26 +39,90 @@ public final class Spell {
 	public static final String TAG_MOD_NAME = "modName";
 	public static final String TAG_MOD_VERSION = "modVersion";
 	private static final String TAG_VALID = "validSpell";
-	public static final StreamCodec<RegistryFriendlyByteBuf, Spell> STREAM_CODEC = new StreamCodec<>() {
-		public Spell decode(RegistryFriendlyByteBuf buf) {
-			return Spell.createFromNBT(buf.readNbt());
-		}
-
-		public void encode(RegistryFriendlyByteBuf buf, Spell pSpell) {
-			CompoundTag cmp = new CompoundTag();
-			if(pSpell != null) {
-				pSpell.writeToNBT(cmp);
-			}
-
-			buf.writeNbt(cmp);
-		}
-	};
 	public final SpellGrid grid = new SpellGrid(this);
 	public String name = "";
 	public UUID uuid;
 
 	public Spell() {
 		uuid = UUID.randomUUID();
+	}
+
+	public record Immutable(String name, UUID uuid, List<ModInformation> modsRequired, SpellGrid grid) {
+		public static Immutable of(String spellName, UUID uuid, SpellGrid grid) {
+			var list = new ArrayList<ModInformation>();
+			var spell = new Spell();
+			spell.name = spellName;
+			spell.uuid = uuid;
+			var immGrid = new SpellGrid(spell);
+			immGrid.gridData = grid.gridData.clone();
+			var imm = new Immutable(spellName, uuid, list, immGrid);
+			list.addAll(imm.getModInformationForCodec());
+			return imm;
+		}
+
+		private static Immutable fromCodecData(boolean valid, String spellName, List<ModInformation> modsRequired, long uuidMost, long uuidLeast, SpellGrid grid) {
+			return new Immutable(spellName, new UUID(uuidMost, uuidLeast), modsRequired, grid);
+		}
+
+		public Set<String> getPieceNamespaces() {
+			Set<String> temp = Collections.newSetFromMap(new HashMap<>());
+			for(SpellPiece[] gridDatum : grid.gridData) {
+				for(SpellPiece spellPiece : gridDatum) {
+					if(spellPiece != null) {
+						temp.add(spellPiece.registryKey.getNamespace());
+					}
+				}
+			}
+			return temp;
+		}
+
+		private List<ModInformation> getModInformationForCodec() {
+			List<ModInformation> info = new ArrayList<>();
+			for(var namespace : this.getPieceNamespaces()) {
+				var optionalMod = ModList.get().getModContainerById(namespace);
+				if(optionalMod.isEmpty()) {
+					continue;
+				}
+				var mod = optionalMod.get();
+				info.add(new ModInformation(mod.getModId(), mod.getModInfo().getVersion().toString()));
+			}
+
+			info.sort(Comparator.comparing(i -> i.name));
+			return info;
+		}
+
+		public Spell mutable() {
+			var spell = new Spell();
+			spell.name = name;
+			spell.uuid = this.uuid;
+			spell.grid.gridData = this.grid.gridData.clone();
+			return spell;
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			// We ignore UUID here as it is usually not meaningful for equality.
+			return this == obj || obj instanceof Immutable imm && this.name.equals(imm.name) && this.grid.equals(imm.grid);
+		}
+
+		public static final MapCodec<Immutable> CODEC = RecordCodecBuilder.mapCodec(instance -> instance.group(
+				Codec.BOOL.fieldOf(TAG_VALID).forGetter(s -> true),
+				Codec.STRING.fieldOf(TAG_SPELL_NAME).forGetter(s -> s.name),
+				Codec.list(ModInformation.CODEC.codec()).fieldOf(TAG_MODS_REQUIRED).forGetter(Immutable::getModInformationForCodec),
+				Codec.LONG.fieldOf(TAG_UUID_MOST).forGetter(s -> s.uuid.getMostSignificantBits()),
+				Codec.LONG.fieldOf(TAG_UUID_LEAST).forGetter(s -> s.uuid.getLeastSignificantBits()),
+				Codec.lazyInitialized(SpellGrid.CODEC::codec).fieldOf("spellList").forGetter(s -> s.grid)
+		).apply(instance, Immutable::fromCodecData));
+
+		public static final StreamCodec<RegistryFriendlyByteBuf, Immutable> STREAM_CODEC = StreamCodec.composite(
+				ByteBufCodecs.BOOL, s -> true,
+				ByteBufCodecs.STRING_UTF8, s -> s.name,
+				ModInformation.STREAM_CODEC.apply(ByteBufCodecs.list()), Immutable::getModInformationForCodec,
+				ByteBufCodecs.VAR_LONG, s -> s.uuid.getMostSignificantBits(),
+				ByteBufCodecs.VAR_LONG, s -> s.uuid.getLeastSignificantBits(),
+				NeoForgeStreamCodecs.lazy(() -> SpellGrid.STREAM_CODEC), s -> s.grid,
+				Immutable::fromCodecData
+		);
 	}
 
 	@Nullable
@@ -101,37 +167,25 @@ public final class Spell {
 		return temp;
 	}
 
-	public static final MapCodec<Spell> CODEC = RecordCodecBuilder.mapCodec(instance -> instance.group(
-			Codec.BOOL.fieldOf(TAG_VALID).forGetter((Spell s) -> true),
-			Codec.STRING.fieldOf(TAG_SPELL_NAME).forGetter((Spell s) -> s.name),
-			Codec.list(ModInformation.CODEC.codec()).fieldOf(TAG_MODS_REQUIRED).forGetter((Spell s) -> {
-				List<ModInformation> info = new ArrayList<>();
-				for(var namespace : s.getPieceNamespaces()) {
-					var optionalMod = ModList.get().getModContainerById(namespace);
-					if(optionalMod.isEmpty()) {
-						continue;
-					}
-					var mod = optionalMod.get();
-					info.add(new ModInformation(mod.getModId(), mod.getModInfo().getVersion().toString()));
-				}
-				info.sort(Comparator.comparing(i -> i.name));
-				return info;
-			}),
-			Codec.LONG.fieldOf(TAG_UUID_MOST).forGetter((Spell s) -> s.uuid.getMostSignificantBits()),
-			Codec.LONG.fieldOf(TAG_UUID_LEAST).forGetter((Spell s) -> s.uuid.getLeastSignificantBits()),
-			Codec.lazyInitialized(SpellGrid.CODEC::codec).fieldOf("spellList").forGetter((Spell s) -> s.grid)
-	).apply(instance, (valid, spellName, modsRequired, uuidMost, uuidLeast, grid) -> {
-		var spell = new Spell();
-		spell.uuid = new UUID(uuidMost, uuidLeast);
-		spell.grid.gridData = grid.gridData;
-		return spell;
-	}));
+	public Immutable immutable() {
+		return Immutable.of(this.name, this.uuid, this.grid);
+	}
+
+	public static final MapCodec<Spell> CODEC = Immutable.CODEC.xmap(Immutable::mutable, Spell::immutable);
+
+	public static final StreamCodec<RegistryFriendlyByteBuf, Spell> STREAM_CODEC = Immutable.STREAM_CODEC.map(Immutable::mutable, Spell::immutable);
 
 	record ModInformation(String name, String version) {
 		public static final MapCodec<ModInformation> CODEC = RecordCodecBuilder.mapCodec(instance -> instance.group(
 				Codec.STRING.fieldOf(TAG_MOD_NAME).forGetter(ModInformation::name),
 				Codec.STRING.fieldOf(TAG_MOD_VERSION).forGetter(ModInformation::version)
 		).apply(instance, ModInformation::new));
+
+		public static final StreamCodec<RegistryFriendlyByteBuf, ModInformation> STREAM_CODEC = StreamCodec.composite(
+				ByteBufCodecs.STRING_UTF8, ModInformation::name,
+				ByteBufCodecs.STRING_UTF8, ModInformation::version,
+				ModInformation::new
+		);
 	}
 
 	public void writeToNBT(CompoundTag cmp) {
@@ -159,4 +213,13 @@ public final class Spell {
 		return createFromNBT(cmp);
 	}
 
+	@Override
+	public boolean equals(Object obj) {
+		return this == obj || obj instanceof Spell oSpell && this.grid.equals(oSpell.grid);
+	}
+
+	@Override
+	public int hashCode() {
+		return super.hashCode();
+	}
 }
--- src/main/java/vazkii/psi/common/item/base/ModDataComponents.java
+++ src/main/java/vazkii/psi/common/item/base/ModDataComponents.java
@@ -6,7 +6,6 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.core.component.DataComponentType;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.core.registries.Registries;
-import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.codec.ByteBufCodecs;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Items;
@@ -15,6 +14,7 @@ import net.neoforged.neoforge.registries.DeferredHolder;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
 import vazkii.psi.api.PsiAPI;
+import vazkii.psi.api.spell.Spell;
 import vazkii.psi.common.core.handler.capability.CADData;
 
 import java.util.List;
@@ -23,7 +23,7 @@ public class ModDataComponents {
 	public static final DeferredRegister.DataComponents DATA_COMPONENT_TYPES = DeferredRegister.createDataComponents(Registries.DATA_COMPONENT_TYPE, PsiAPI.MOD_ID);
 	public static final DeferredHolder<DataComponentType<?>, DataComponentType<BlockPos>> DST_POS = DATA_COMPONENT_TYPES.registerComponentType("dst_x", builder -> builder.persistent(BlockPos.CODEC).networkSynchronized(BlockPos.STREAM_CODEC));
 	public static final DeferredHolder<DataComponentType<?>, DataComponentType<BlockPos>> SRC_POS = DATA_COMPONENT_TYPES.registerComponentType("src_z", builder -> builder.persistent(BlockPos.CODEC).networkSynchronized(BlockPos.STREAM_CODEC));
-	public static final DeferredHolder<DataComponentType<?>, DataComponentType<CompoundTag>> SPELL = DATA_COMPONENT_TYPES.registerComponentType("spell", builder -> builder.persistent(CompoundTag.CODEC).networkSynchronized(ByteBufCodecs.COMPOUND_TAG));
+	public static final DeferredHolder<DataComponentType<?>, DataComponentType<Spell>> SPELL = DATA_COMPONENT_TYPES.registerComponentType("spell", builder -> builder.persistent(Spell.CODEC.codec()).networkSynchronized(Spell.STREAM_CODEC));
 	public static final DeferredHolder<DataComponentType<?>, DataComponentType<Item>> SENSOR = DATA_COMPONENT_TYPES.registerComponentType("sensor", builder -> builder.persistent(BuiltInRegistries.ITEM.byNameCodec().orElse(Items.AIR)).networkSynchronized(ByteBufCodecs.registry(Registries.ITEM)).cacheEncoding());
 	public static final DeferredHolder<DataComponentType<?>, DataComponentType<Integer>> TIMES_CAST = DATA_COMPONENT_TYPES.registerComponentType("times_cast", builder -> builder.persistent(Codec.INT).networkSynchronized(ByteBufCodecs.INT));
 	public static final DeferredHolder<DataComponentType<?>, DataComponentType<Integer>> SELECTED_CONTROL_SLOT = DATA_COMPONENT_TYPES.registerComponentType("selected_control_slot", builder -> builder.persistent(Codec.INT).networkSynchronized(ByteBufCodecs.INT));
